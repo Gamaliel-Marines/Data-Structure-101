1eabf0ec6fb0d3652496c31407caf5fec58351f2
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2024-02-14 23:22:19 -0600

    Feat: hash set

--- data structure & algorithms interviews/hash_set.cpp
+++ data structure & algorithms interviews/hash_set.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+
 using namespace std;
 
 
@@ -16,10 +17,26 @@ int findUnpaired(vector<int>& nums) {
     return *s.begin();
 }
 
+string findUnpaired(vector<string>& nums) {
+    unordered_set<string> s;
+    for (string i : nums) {
+        if (s.find(i) != s.end()) {
+            s.erase(i);
+        } else {
+            s.insert(i);
+        }
+    }
+    return *s.begin();
+}
+
+
 
 
 int main() {
     vector<int> v = {1, 6, 3, 2, 8, 3, 2, 1, 6};
     cout << findUnpaired(v) << endl;
+
+    vector<string> v2 = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "a", "b", "c", "d", "e", "f", "g", "h", "i"};
+    cout << findUnpaired(v2) << endl;
     return 0;
 }
